41e589ef93451b5942ac39e4cdb68fdcc5729a5e
sdmule <mulesaurabh45@gmail.com>
2026-06-23 17:33:47 +0530

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DapperDemo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DapperDemo.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Company> Companies { get; set; }
+    }
+}
