aa8a6e1d306afc94e77779d0fee8beb1d511efaf
sdmule <mulesaurabh45@gmail.com>
2026-06-24 11:46:41 +0530

    Implemented EntityFramework Repository

---
+++ Repository/CompanyRepositoryEF.cs
@@ -0,0 +1,42 @@
+﻿using DapperDemo.Data;
+using DapperDemo.Models;
+
+namespace DapperDemo.Repository
+{
+    public class CompanyRepositoryEF : ICompanyRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CompanyRepositoryEF(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public Company Add(Company company)
+        {
+            _db.Companies.Add(company);
+            _db.SaveChanges();
+            return company;
+        }
+        public Company Find(int id)
+        {
+            return _db.Companies.FirstOrDefault(u => u.CompanyId == id);
+        }
+        public List<Company> FindAll()
+        {
+            return _db.Companies.ToList();
+        }
+        public void Delete(int id)
+        {
+            Company company = _db.Companies.FirstOrDefault(u => u.CompanyId == id);
+            _db.Companies.Remove(company);
+            _db.SaveChanges();
+            return;
+        }
+        public Company Update(Company company)
+        {
+            _db.Companies.Update(company);
+            _db.SaveChanges();
+            return company;
+        }
+    }
+}
