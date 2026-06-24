670c7427e806abc65b6217e57d381be29a8ed61d
sdmule <mulesaurabh45@gmail.com>
2026-06-24 22:34:29 +0530

    Changes of Stored Procedures to get Company info

---
+++ Repository/CompanyRepositorySP.cs
@@ -0,0 +1,47 @@
+﻿using Dapper;
+using DapperDemo.Data;
+using DapperDemo.Models;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace DapperDemo.Repository
+{
+    public class CompanyRepositorySP : ICompanyRepository
+    {
+        private IDbConnection db;
+
+        public CompanyRepositorySP(IConfiguration configuration)
+        {
+            this.db = new SqlConnection(configuration.GetConnectionString("DefaultConnection"));
+        }
+        public Company Add(Company company)
+        {
+            var sql = "INSERT INTO Companies (Name, Address, City, State, PostalCode) VALUES(@Name, @Address, @City, @State, @PostalCode);"
+                        + "SELECT CAST(SCOPE_IDENTITY() as int);";
+            var id = db.Query<int>(sql, company).Single();
+            company.CompanyId = id;
+            return company;
+        }
+        public Company Find(int id)
+        {
+            return db.Query<Company>("usp_GetCompany", new { CompanyId = id }, commandType: CommandType.StoredProcedure).Single();
+        }
+        public List<Company> FindAll()
+        {
+            return db.Query<Company>("usp_GetALLCompany", commandType: CommandType.StoredProcedure).ToList();
+        }
+        public void Delete(int id)
+        {
+            var sql = "DELETE FROM Companies WHERE CompanyId = @Id";
+            db.Execute(sql, new { Id = id });
+            return;
+        }
+        public Company Update(Company company)
+        {
+            var sql = "UPDATE Companies SET Name = @Name, Address = @Address, City = @City, " +
+                     "State = @State, PostalCode = @PostalCode WHERE CompanyId = @CompanyId";
+            db.Execute(sql, company);
+            return company;
+        }
+    }
+}
